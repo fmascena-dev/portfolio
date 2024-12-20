af063ea6d61e040275d4d66cbbeeef28ec961437
felipe mascena <felipe.mascena.dev@gmail.com>
2024-12-20 15:08:31 -0300

    iniciando a seção Sobre Mim

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Tecnologias from './components/Tecnologias/Tecnologias.jsx';
 import Contato from './components/Contato/Contato.jsx';
 import Projetos from './components/Projetos/Projetos.jsx';
+import Sobre from './components/Sobre/Sobre.jsx';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
         <Route path="/tecnologias" element={<Tecnologias />} />
         <Route path="/projetos" element={<Projetos />} />
         <Route path="/contato" element={<Contato />} />
-        {/* <Route path="/sobre" element={<SobreMim />} /> */}
+        <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Sobre/Sobre.jsx
@@ -0,0 +1,21 @@
+export default function Sobre() {
+    return (
+      <section id="sobre">
+        <h2>Sobre mim</h2>
+        <p>
+          Venho de uma carreira na área de logística, onde atuei como conferente
+          e posteriormente como encarregado geral. Nessas posições, desenvolvi
+          habilidades essenciais como análise crítica, pensamento estratégico e
+          a capacidade de encontrar soluções rápidas para problemas. Hoje,
+          aplico essas competências na minha jornada como desenvolvedor web
+          frontend. Com uma base sólida em HTML, CSS, Sass, Styled-components,
+          JavaScript, React e Vue.js, e atualmente aprofundando meus
+          conhecimentos em TypeScript e Angular, estou sempre pronto para
+          aprender e enfrentar novos desafios. Estou cursando Análise e
+          Desenvolvimento de Sistemas e Engenharia de Software, buscando
+          continuamente melhorar minhas habilidades e contribuir efetivamente
+          para projetos inovadores.
+        </p>
+      </section>
+    );
+}
