2220b8ed2e3fbb2bd1f4d7517c6473844c996526
felipe mascena <felipe.mascena.dev@gmail.com>
2024-12-22 20:03:27 -0300

    finalizando a responsividade do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
+import { FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import Data from '../../Const';
 import './FooterStyles.scss';
+import { MdOutlineEmail } from 'react-icons/md';
 
 export default function Footer() {
   const comeco = 'Desenvolvido por ';
@@ -12,6 +15,35 @@ export default function Footer() {
           <span>{frase}</span>
         </p>
       </div>
+      <div className="contacts">
+              <a
+                href={Data.WhatsappLink}
+                rel="noreferrer"
+                target="_blank"
+                title="Whatsapp"
+              >
+                <FaWhatsapp />
+              </a>
+              <a
+                href={Data.LinkedinLink}
+                rel="noreferrer"
+                target="_blank"
+                title="Linkedin"
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href={Data.GitHubLink}
+                rel="noreferrer"
+                target="_blank"
+                title="Github"
+              >
+                <FaGithub />
+              </a>
+              <a href={Data.EmailLink} rel="noreferrer" target="_blank" title="Gmail">
+                <MdOutlineEmail />
+              </a>
+            </div>
     </footer>
   );
 }
